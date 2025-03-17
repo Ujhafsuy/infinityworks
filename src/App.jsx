ff7c6f010543318340fa5058a97807c6dfc8ac85
Ujhafsuy <duardoescubemaya@gmail.com>
2025-03-17 11:53:37 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import seta from './assets/arrow_forward.png'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1>The<br/> Infinity Works <br/>Team Homepage</h1>
+        <button>Entrar <img src={seta} alt="" /></button>
+      </div>
+    </>
+  )
+}
+
+export default App
