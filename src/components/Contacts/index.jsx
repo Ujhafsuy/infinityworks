fbdf859f1fe10b318c9bc5fe343a6730eec4f653
Pedro Henrique Reis Lima <160922340+Pedro-Pratina@users.noreply.github.com>
2025-07-02 21:03:48 -0300

    Não sei Me perdi, quando comecei eu e Deus sabia, agora só Deus sabe .-.

--- src/components/Contacts/index.jsx
+++ src/components/Contacts/index.jsx
@@ -3,36 +3,33 @@ import './style.css'
 
 function Contacts() {
     return (
-        <section id='Contatos' className='text-white flex flex-col gap-[6.6875rem]'>
-            <h2 className='text-[4rem]'>Contato:</h2>
-            <div className='flex flex-wrap items-start gap-10 justify-between'>
+        <section id='Contatos' className='text-white flex flex-col'>
+            <h2 className='text-[2rem] font-[700] text-[var(--text-color)]'>Contato:</h2>
+            <div className='ajusta-container'>
                 <div className='flex flex-col gap-2'>
-                    <p className='text-[3rem]'>Você pode nos<br /> encontrar nas<br /> seguintes<br /> plataformas:</p>
+                    <p className='text-[1.5rem]'>
+                        Você pode nos<br />
+                        encontrar nas<br />
+                        seguintes<br />
+                        plataformas:
+                    </p>
 
                     <div className='flex flex-col gap-1'>
-                        <a href="#" className='block text-[2rem] text-lg font-bold bg-gradient-to-r from-amber-300 via-pink-600 to-purple-700 bg-clip-text text-transparent'>
+                        <a href="#" className='block text-[1.125rem] text-lg font-bold bg-gradient-to-r from-amber-300 via-pink-600 to-purple-700 bg-clip-text text-transparent'>
                             Nosso Instagram
                         </a>
 
-                        <a href="#" className='block text-[2rem] text-lg font-bold bg-gradient-to-r from-[#93EBFF] to-[#00D0FF] bg-clip-text text-transparent'>
-                            Nosso Twitter/X
-                        </a>
-
-                        <a href="#" className='block text-[2rem] text-lg font-bold text-[#2D69FF]'>
+                        <a href="#" className='block text-[1.125rem] text-lg font-bold text-[#2D69FF]'>
                             Nosso LinkedIn
                         </a>
 
-                        <a href="#" className='block text-[2rem] text-lg font-bold bg-gradient-to-r from-[#702AFF] to-white bg-clip-text text-transparent'>
+                        <a href="#" className='block text-[1.125rem] text-lg font-bold bg-gradient-to-r from-[#702AFF] to-white bg-clip-text text-transparent'>
                             Nosso GitHub
-                        </a>
-
-                        <a href="#" className='block text-[2rem] text-lg font-bold'>
-                            Nosso <span className='text-red-500'>YouTube</span>
                         </a>
                     </div>
                 </div>
 
-                <img src={FotoTop} alt='Foto dos membros da Infinit Works, posicionados com o celular na mão' className='max-w-[300px] object-contain' />
+                <img src={FotoTop} alt='Foto dos membros da Infinit Works, posicionados com o celular na mão' className='max-w-[18.75rem] object-contain' />
             </div>
         </section>
     )
